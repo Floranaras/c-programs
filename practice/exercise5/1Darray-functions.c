5aac430fd7723f3f591cbd24b89ed90e5af09d0c
callo <callo@callos-MacBook-Air.local>
2025-05-04 16:11:54 +0800

    feat(exercise5/):completed

---
+++ practice/exercise5/1Darray-functions.c
@@ -0,0 +1,100 @@
+#define MAX (5)
+
+int isInList(int *list)
+{
+    int i = 0, flag = 0, key;
+
+    printf("Input key: ");
+    scanf("%d", &key);
+
+    while (i < MAX && !flag)
+    {
+        if (key == list[i])
+            flag = 1;
+        i++;
+    }
+
+    return flag;
+}
+
+int getMaxValue(int *list)
+{
+    int max = list[0];
+    int i;
+
+    for (i = 0; i < MAX; i++)
+    {
+        if (list[i] > max)
+            max = list[i];
+    }
+
+    return max;
+}
+
+void getIdxSwap(int *idx, int *swap)
+{
+    printf("Input index: ");
+    scanf("%d", idx);  
+    printf("Enter data: ");
+    scanf("%d", swap);
+}
+void replaceDataAt(int *list)
+{
+    int idx, swap;
+
+    getIdxSwap(&idx,&swap);
+    list[idx] = swap;
+}
+
+void printArray(int *list)
+{
+    int i;
+
+    printf("Values of the Array:\n");
+
+    for (i = 0; i < MAX; i++)
+        printf("%d\n", list[i]);
+}
+
+void addDataAt(int *list)
+{
+    int idx, swap, i;
+
+    getIdxSwap(&idx,&swap);
+
+    //Adjust it by one
+    for (i = MAX; i > idx; i--)
+        list[i] = list[i - 1];
+
+    list[idx] = swap; 
+}
+
+void deleteData(int *list)
+{
+    int idx, i;
+    printf("Input index: ");
+    scanf("%d", &idx);
+
+    for (i = idx; i > 0; i--)
+        list[i] = list[i - 1];
+
+    list[0] = 0;
+}
+
+void sortAsc(int *list)
+{
+    int i, j, temp;
+
+    for (i = 0; i < MAX; i++)
+    {
+        for (j = 0; j < MAX; j++)
+        {
+            if (list[i] < list[j])
+            {
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+        }
+    }
+}
---
+++ practice/exercise5/1Darray-main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "1Darray-functions.c"
+
+int main()
+{
+    int list[MAX] = {1, 2, 3, 4, 5};
+
+    //isInList checker
+    if (isInList(list) == 1)
+        printf("key is found\n");
+    else
+        printf("key is not found\n");
+
+    //getMaxValue checker
+    printf("The highest value is %d\n", getMaxValue(list));
+
+    //replaceDataAt checker
+    printArray(list);
+    replaceDataAt(list);
+    printArray(list);
+
+    //addDataAt checker
+    addDataAt(list); 
+    printArray(list);
+    //deleteData checker
+    deleteData(list);
+    printArray(list);
+
+    //sortAsc checker
+    sortAsc(list);
+    printArray(list);
+
+    return 0;
+}
