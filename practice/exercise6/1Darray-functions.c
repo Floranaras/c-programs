befbfa1b5dede2718329b7fcbbbf7fc30665d1b7
CarlosRanara <carlosranara0@gmail.com>
2025-05-08 07:22:35 +0800

    feat(excersie6/): finished addUnique func

--- practice/exercise6/1Darray-functions.c
+++ practice/exercise6/1Darray-functions.c
@@ -14,3 +14,32 @@ int isEqual(int *src, int *dst, int size)
     return flag;
 }
 
+void removeDuplicates()
+{
+
+}
+
+void addUnique(int* src, int* dst, int* size)
+{
+	int i, j, found = 0;
+
+	for (i = 0; i < *size; i++)
+	{
+		found = 0;
+
+		for (j = 0; j < *size; j++)
+		{
+			if (src[i] == dst[j])
+			{
+				found = 1;
+			}
+		}
+		
+		if (!found)
+		{
+			dst[*size - 1] = src[i];
+			*(size)++;
+		}
+
+	}
+}
