bdbb763a1d0b440989d1c4ab6046273d9e37b767
Carlos Ranara <carlosranara0@gmail.com>
2025-05-11 12:55:04 +0800

    mystring.c finished

---
+++ practice/strings/mystring-test.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#define STR "CAR" 
+
+int main() {
+    char str1[4] = {'C','A','T'};
+    char str2[4] = {'D','O','G'};
+    char str10[11] = {'N','E','K','O'};
+
+    printf("\n===STRLEN TEST===\n");
+    printf("string.h: %d\n", strlen(STR));
+    printf("mystring.c: %d\n", mystrlen(STR));
+
+    printf("\n===STRCPY TEST===\n");
+    strcpy(str1, STR);
+    printf("string.h: %s\n", str1);
+    mystrcpy(str2, str1);
+    printf("mystring.c: %s\n",str1);
+
+    printf("\n===STRCAT TEST===\n");
+    strcat(str10, STR);
+    printf("string.h: %s\n", str10);
+    mystrcpy(str10,"NEKO");
+    mystrcat(str10, STR);
+    printf("mystring.c: %s\n",str10);
+
+
+    printf("\n===STRCMP TEST===\n");
+    printf("string.h: %d\n", strcmp("NEKO", "CAT"));
+    printf("mystring.c: %d\n",mystrcmp("NEKO", "CAT"));
+
+    return 0;
+}
---
+++ practice/strings/mystring.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int mystrlen(char *str)
+{
+    int i = 0;
+    
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+
+    return i;
+}
+
+char *mystrcpy(char *dest, char *src)
+{
+    int i = 0;
+
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+
+    dest[i] = '\0';
+    
+    return dest;
+}
+
+char *mystrcat(char *dest, char *src)
+{
+    int start = mystrlen(dest);
+    int i = 0;
+
+    while (src[i] != '\0')
+    {
+        dest[start + i] = src[i];
+        i++;
+    }
+
+    return dest;
+}
+
+int mystrcmp(char *str1, char *str2)
+{
+
+    int i = 0, flag = 0;
+
+
+    while (str1[i] == '\0' && str2[i] == '\0' && !flag)
+    {
+        if (str1[i] != str2[i])
+        {
+            flag = 1;
+        }
+
+        i++; 
+    }
+
+    return flag = 1 ? str1[i] - str2[i] : 0;
+}
+
+#include "mystring-test.c"
