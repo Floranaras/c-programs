03a4958516c799e7eabfbda827e117fb87c6c86b
Carlos Ranara <carlosranara0@gmail.com>
2025-05-16 18:31:28 +0800

    exercise 6 finished

--- practice/exercise6/1Darray-functions.c
+++ practice/exercise6/1Darray-functions.c
@@ -1,3 +1,27 @@
+#define SIZE (5)
+#define MAX (10)
+
+void printArr(int* A, int size)
+{
+	int j;
+
+	for (j = 0; j < size; j++)
+	{
+		printf("%d ", A[j]);
+	}
+
+	printf("\n");
+}
+
+void initArr(int* A)
+{
+	int j;
+
+	for (j = SIZE; j < MAX; j++)
+	{
+		A[j] = 0;
+	}
+}
 
 int isEqual(int *src, int *dst, int size)
 {
@@ -14,20 +38,43 @@ int isEqual(int *src, int *dst, int size)
     return flag;
 }
 
-void removeDuplicates()
+void shiftElements (int *A, int size, int idx)
 {
+	int j;
 
+	for (j = idx; j < size - 1; j++)
+	{
+		A[j] = A[j+1];
+	}
+
+	A[j] = 0;
 }
 
-void addUnique(int* src, int* dst, int* size)
+void removeDuplicates(int* A, int size)
+{
+	int j, k;
+	
+	for (j = 0; j < size; j++)
+	{
+		for (k = j + 1; k < size; k++)
+		{
+			if (A[j] == A[k])	
+			{
+				shiftElements(A, size, k);
+			}
+		}
+	}
+}
+
+void addUnique(int* src, int* dst, int size)
 {
 	int i, j, found = 0;
 
-	for (i = 0; i < *size; i++)
+	for (i = 0; i < size; i++)
 	{
 		found = 0;
 
-		for (j = 0; j < *size; j++)
+		for (j = 0; j < size; j++)
 		{
 			if (src[i] == dst[j])
 			{
@@ -37,8 +84,8 @@ void addUnique(int* src, int* dst, int* size)
 		
 		if (!found)
 		{
-			dst[*size - 1] = src[i];
-			*(size)++;
+			dst[size - 1] = src[i];
+			size++;
 		}
 
 	}
--- practice/exercise6/1Darray-main.c
+++ practice/exercise6/1Darray-main.c
@@ -3,17 +3,32 @@
 
 int main()
 {
-    int size = 5;
-    int src[] = {2, 6, 10, 29, 5};
-    int dst[] = {3, 6, 11, 17, 2};
+    int src[MAX] = {2, 6, 10, 29, 5};
+    int dst[MAX] = {3, 6, 11, 17, 2};
+    int test[] = {2, 3, 5, 2, 55};
 
     // isEqual checker
-    if (isEqual(src, dst, size))
+    printf("\nisEqual checker:\n");
+    if (isEqual(src, dst, SIZE))
         printf("The arrays are equal\n");
     else 
         printf("The arrays are not equal\n");
 
-    //
+    // removeDuplicates checker 
+    removeDuplicates(test, SIZE);
+    printf("\nremoveDuplicate checker:\n");
+    printArr(test, SIZE);
+
+    // addUnique checker
+    initArr(src);
+    initArr(dst);
+    addUnique(src,dst,SIZE);
+
+    printf("\naddUnique checker:\n");
+    printf("src Array: ");
+    printArr(src, MAX);
+    printf("dst Array: ");
+    printArr(dst, MAX);
     
     return 0;
 }
