d3bf8e8434cc5ccb3ef78ebfff372e3eed6e79aa
Carlos Ranara <carlosranara0@gmail.com>
2025-05-19 12:19:45 +0800

    updated arraytest.c

--- practice/exercise3/arraytest.c
+++ practice/exercise3/arraytest.c
@@ -7,18 +7,21 @@ int main()
     int A[SIZE];
     int j = 0, flag = 0;
     int k = 0;
-    int num;
+    int nNum;
+    int nTotal = 0;
+    float fAvg = 0;
+    int values[SIZE];
 
     // Input
     printf("Enter 15 positive numbers\n");
   
     do 
     {
-        printf("Enter value: ");
-        scanf("%d", &num);
-        if (num > 0)
+        printf("Enter a number: ");
+        scanf("%d", &nNum);
+        if (nNum > 0)
         {
-            A[j] = num;
+            A[j] = nNum;
             j++;
         }
 
@@ -36,10 +39,26 @@ int main()
     while (k < j)
     {
         printf("%d ", A[k]);
+        nTotal += A[k];
         k++;
     }
     
     printf("\n");
+
+    fAvg = nTotal*(1.0) / k;
+    printf("Average: %.2f\n", fAvg);
+
+    k = 0;
+
+    for (k = 0; k < SIZE; k++)
+    {
+        values[k] = 0;
+    }
+
+    for (k = 0; k < j; k++)
+    {
+        values[k] = A[k];
+    }
 
     return 0;
 }
