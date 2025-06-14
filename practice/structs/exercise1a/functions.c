d576394a200c563de43cf75989a939f8179ec71a
Carlos Ranara <carlosranara0@gmail.com>
2025-06-15 07:57:16 +0800

    structs exercise done

--- practice/structs/exercise1a/functions.c
+++ practice/structs/exercise1a/functions.c
@@ -10,32 +10,47 @@ void inputNewUser (databaseType *db)
 	char choice = 'y';
 	userType *currentUser = &db->users[db->userCount];
 	int *vaxCtr = &currentUser->vaxCount;
-	vaxType *vaxList = &currentUser->vaccines;
+	vaxType *vaxList = currentUser->vaccines;
 
 	if (db->userCount != MAX_PEOPLE)
 	{
+		printf("\n===Enter name of user===\n");
+		printf("Enter firstname: ");
 		scanf("%s", currentUser->name.first);
+		printf("Enter lastname: ");
 		scanf("%s", currentUser->name.last);
-		scanf("%c", &currentUser->name.middle);
+		printf("Enter middle initial: ");
+		scanf(" %c", &currentUser->name.middle);
 
+		printf("\n===Enter day of birth===\n");
+		printf("Enter month: ");
 		scanf("%d", &currentUser->birthDate.month);
+		printf("Enter day: ");
 		scanf("%d", &currentUser->birthDate.day);
+		printf("Enter year: ");
 		scanf("%d", &currentUser->birthDate.year);
 		
 		*vaxCtr = 0;
 
 		for (j = 0; j < MAX_VAX && choice == 'y'; j++)	
 		{
+			printf("\n===Enter vaccine information===\n");
+			printf("Enter brand: ");
 			scanf("%s",vaxList[j].brand);
+			printf("Enter city where vaccination was administered: ");
 			scanf("%s",vaxList[j].city);
-
+					
+			printf("\n===Enter date of when vaccination was administered===\n");
+			printf("Enter month: ");
 			scanf("%d",&vaxList[j].vaxDate.month);
+			printf("Enter day: ");	
 			scanf("%d",&vaxList[j].vaxDate.day);
+			printf("Enter year: ");
 			scanf("%d",&vaxList[j].vaxDate.year);
 
 			(*vaxCtr)++;
-
-			scanf(" %c", choice);
+			printf("Do you want to enter another vaccine? (y/n): ");
+			scanf(" %c", &choice);
 		}
 
 		db->userCount++;
@@ -48,19 +63,93 @@ void inputNewUser (databaseType *db)
 
 }
 
-void displayUserData (databaseType *db)
+void displayUserData (databaseType *db, int idx)
 {
+	int j;
+	userType *user = &db->users[idx];
+	vaxType *vax = db->users[idx].vaccines;
 
+	printf("\nName: %s %c. %s\n", user->name.first, 
+								user->name.middle, 
+								user->name.last);
+	printf("Birth Date: %2d/%2d/%2d\n", user->birthDate.month,
+									user->birthDate.day,
+									user->birthDate.year);
+
+	for (j = 0; j < user->vaxCount; j++)
+	{
+		printf("\n===Vaccine #%d===\n", j + 1);
+		printf("Vaccine Brand: %s\n", vax[j].brand);
+		printf("City of Vaccination: %s\n", vax[j].city);
+		printf("Date of Vaccination: %2d/%2d/%2d\n", vax[j].vaxDate.month,
+													vax[j].vaxDate.day,
+													vax[j].vaxDate.year);
+	}	
 }
 
 void searchByName (databaseType *db)
 {
+	nameType input;
+	int j;
+	int isFound = 0;
+
+	printf("\n===Search User by Name===\n");
+	printf("\n===Enter name of user===\n");
+	printf("Enter firstname: ");	
+	scanf("%s", input.first);
+	printf("Enter lastname: ");	
+	scanf("%s", input.last);
+	printf("Enter middle initial: ");	
+	scanf(" %c", &input.middle);
+
+	for (j = 0; j < db->userCount && !isFound; j++)	
+	{
+		if (strcmp(input.first, db->users[j].name.first) == 0 && 
+			strcmp(input.last, db->users[j].name.last) == 0 &&
+			input.middle == db->users[j].name.middle)
+		{
+			isFound = 1;
+		}
+	}
+
+	if (isFound)
+		displayUserData(db, j-1);
+	else 
+		printf("User not found!\n");
 
 }
 
 void searchByDate (databaseType *db)
 {
+	dateType input;
+	int j;
+	int isFound = 0;
+	int ctr = 0;
 
+	printf("\n===Search User by Birthdate===\n");
+	printf("\n===Enter birthdate of user===\n");
+	printf("Enter month: ");	
+	scanf("%d", input.month);
+	printf("Enter day: ");	
+	scanf("%d", input.day);
+	printf("Enter year: ");	
+	scanf("%d", &input.year);
+
+	for (j = 0; j < db->userCount; j++)	
+	{
+		if (input.month == db->users[j].birthDate.month &&
+			input.day == db->users[j].birthDate.day &&
+			input.year == db->users[j].birthDate.year)
+		{
+			displayUserData(db, j);
+			ctr++;
+		}
+	}
+
+	if (!ctr)
+	{
+		printf("User Not Found!\n");
+	}
 }
 
 void mainMenu (databaseType *db)
